686b317036f66388885aa3152250a9423ce2d3f3
Austinhamilton1 <95155318+Austinhamilton1@users.noreply.github.com>
2024-06-17 15:07:43 -0400

    Add files via upload
    Added a way to parse a dynamic graph as a gexf file

--- data_parser.py
+++ data_parser.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import os
 from math import radians, sin, cos, asin, sqrt, isnan
-import networkx as nx
+import dynetx as dn
 from tqdm import tqdm
 import csv
+from gexf import Node, Edge, Graph, Gexf
 
 def find_file(base_path: str, filename: str) -> str:
     '''
@@ -92,7 +93,7 @@ def earth_distance(lat_lon_1: 'tuple[float]', lat_lon_2: 'tuple[float]'):
 
     return c * R
     
-def generate_dynamic_graph(car_data: pd.DataFrame, threshold: float=15.0) -> nx.Graph:
+def generate_dynamic_graph(car_data: pd.DataFrame, threshold: float=15.0) -> dn.DynGraph:
     '''
     Generates a dynamic graph from car data. This data should be collected and merged prior
     to being used as input. The algorithm adds a node for each car at each timestep if the 
@@ -100,55 +101,51 @@ def generate_dynamic_graph(car_data: pd.DataFrame, threshold: float=15.0) -> nx.
     each pair of cars and if the distance is less than or equal to threshold, it creates an
     edge between the two cars
     '''
-    dynamic_graph = nx.Graph()
-    #this will be used as the label for each new node, we start at 0
-    current_node = 0
+    dynamic_graph = dn.DynGraph(edge_removal=True)
     #tqdm adds a progress bar
-    for i in tqdm(range(len(car_data.index))):
+    for i in tqdm(range(0, len(car_data.index), 15)):
         #this is to make sure we don't add any nodes twice
-        added_nodes = {}
         lat_lon_1 = (car_data['ID1_latitude'].iloc[i], car_data['ID1_longitude'].iloc[i])
         lat_lon_2 = (car_data['ID2_latitude'].iloc[i], car_data['ID2_longitude'].iloc[i])
         lat_lon_3 = (car_data['ID3_latitude'].iloc[i], car_data['ID3_longitude'].iloc[i])
         lat_lon_4 = (car_data['ID4_latitude'].iloc[i], car_data['ID4_longitude'].iloc[i])
         positions = [lat_lon_1, lat_lon_2, lat_lon_3, lat_lon_4]
         #check every pair of lat/lon
-        for j, position in enumerate(positions):
-            for k, other_position in enumerate(positions[j+1:]):
+        for j in range(len(positions)-1):
+            position = positions[j]
+            for k in range(j+1, len(positions)):
+                other_position = positions[k]
                 #if the type of lat/lon is str, it is not defined for the first car
                 #so we don't want to consider any pairs involving this car
                 #(the second car will be considered again in the outer loop)
                 if type(position[0]) == str or type(position[1]) == str:
                     break
-                #if we haven't already added the node, go ahead and add it
-                if position not in added_nodes:
-                    dynamic_graph.add_node(current_node, speed=car_data[f'ID{j+1}_speed'].iloc[i], heading=car_data[f'ID{j+1}_heading'].iloc[i])
-                    node_i = current_node
-                    added_nodes[position] = node_i
-                    current_node += 1
-                #otherwise, just get the previously added node
-                else:
-                    node_i = added_nodes[position]
                 #if the type of other lat/lon is a str, it is not defined, and we
                 #need to skip over that position
                 if type(other_position[0]) == str or type(other_position[1]) == str:
                     continue
-                #if the other_position is not in added_nodes, add a new node
-                if other_position not in added_nodes:
-                    dynamic_graph.add_node(current_node, speed=car_data[f'ID{k+1}_speed'].iloc[i], heading=car_data[f'ID{k+1}_heading'].iloc[i])
-                    node_j = current_node
-                    added_nodes[other_position] = node_j
-                    current_node += 1
-                #if the other_position is in added_nodes, lookup the node value
-                else:
-                    node_j = added_nodes[other_position]
                 #get the earth_distance (using the haversine formula) between the two positions
                 distance = earth_distance(position, other_position)
                 #if the distance between the two cars is less than or equal to our threshold,
                 #add an edge between the two nodes associated with those positions
                 if distance <= threshold:
-                    dynamic_graph.add_edge(node_i, node_j, time=df['gpstime'].iloc[i])
+                    dynamic_graph.add_interaction(u=j, v=k, t=df['gpstime'].iloc[i])
     return dynamic_graph
+
+def write_dynamic_graph(dynamic_graph: dn.DynGraph, filename: str) -> None:
+    '''
+    Writes a dynamic graph to a gexf file using helper functions 
+    '''
+    graph = Graph(mode='dynamic')
+    for node in dynamic_graph.nodes(data=True):
+        graph.add_node(node[0]) 
+    for edge in dynamic_graph.interactions():
+        for timestamp in edge[2]['t']:
+            graph.add_edge(source=edge[0], target=edge[1], timestamp=timestamp[0])
+    gexf = Gexf('Austin Hamilton', 'Dynamic VSN Data')
+    gexf.load_graph(graph)
+    with open(filename, 'w') as file:
+        file.write(str(gexf))
 
 id_1 = collect_data('ID1')
 id_2 = collect_data('ID2', 'audi')
@@ -158,4 +155,4 @@ id_4 = collect_data('ID4', 'fiat')
 df = merge_data((id_1, id_2, id_3, id_4), on='gpstime')
 
 graph = generate_dynamic_graph(df)
-nx.write_gexf(graph, 'graph.gexf')
+write_dynamic_graph(graph, 'graph.gexf')
